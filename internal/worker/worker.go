f8a94f99ff1a204233f91563001dbfde3d905e10
huwenbin.77 <huwenbin.77@bytedance.com>
2026-04-02 19:02:07 +0800

    feat: implement cron worker

---
+++ internal/worker/worker.go
@@ -0,0 +1,47 @@
+package worker
+
+import (
+	"log"
+
+	"code.byted.org/fintech_cf/common-notify/internal/service"
+	"github.com/robfig/cron/v3"
+)
+
+// Worker 定时任务 worker
+type Worker struct {
+	cron                *cron.Cron
+	notificationService *service.NotificationService
+}
+
+// NewWorker 创建 worker
+func NewWorker(ns *service.NotificationService) *Worker {
+	return &Worker{
+		cron:                cron.New(),
+		notificationService: ns,
+	}
+}
+
+// Start 启动 worker
+func (w *Worker) Start() error {
+	// 每 10 秒执行一次
+	_, err := w.cron.AddFunc("*/10 * * * * *", func() {
+		if err := w.notificationService.ProcessPendingNotifications(100); err != nil {
+			log.Printf("error processing notifications: %v", err)
+		}
+	})
+
+	if err != nil {
+		return err
+	}
+
+	w.cron.Start()
+	log.Println("worker started")
+	return nil
+}
+
+// Stop 停止 worker
+func (w *Worker) Stop() {
+	ctx := w.cron.Stop()
+	<-ctx.Done()
+	log.Println("worker stopped")
+}
